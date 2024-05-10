9e80fdd1270d064c947025f36f3ec844380e3547
Kumaran98 <rajeswaranilankumaran@gmail.com>
2024-05-10 14:17:17 +0530

    Intial Commit

---
+++ src/main/java/com/task/TaskAllocation/TaskAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.task.TaskAllocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAllocationApplication.class, args);
+	}
+
+}
