62075461a1d37b3aca19947f899ff321be5349f6
Kumaran98 <rajeswaranilankumaran@gmail.com>
2024-05-15 01:41:13 +0530

    Added the JAR file

--- src/main/java/com/task/TaskAllocation/Controller/TeamMembersController.java
+++ src/main/java/com/task/TaskAllocation/Controller/TeamMembersController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping(EndPointBundle.TEAMMEMBERS)
 public class TeamMembersController {
--- src/main/java/com/task/TaskAllocation/TaskAllocationApplication.java
+++ src/main/java/com/task/TaskAllocation/TaskAllocationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskAllocationApplication {
+public class  TaskAllocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskAllocationApplication.class, args);
